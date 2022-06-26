bdc538bb79377dbd3742c3f34dd5c3c366faf344
zansangeeth <zansangeeth17@gmail.com>
2022-06-27 00:08:49 +0530

    after home animations navigate to success fragment

--- app/src/main/java/com/zasa/hashr/HomeFragment.kt
+++ app/src/main/java/com/zasa/hashr/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.*
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.zasa.hashr.databinding.FragmentHomeBinding
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -30,6 +31,7 @@ class HomeFragment : Fragment() {
         binding.btnGenerate.setOnClickListener {
             lifecycleScope.launch{
                 applyAnimations()
+                navigateToSuccess()
             }
         }
         return binding.root
@@ -56,6 +58,15 @@ class HomeFragment : Fragment() {
         binding.vSuccess.animate().rotationBy(720f).duration = 600L
         binding.vSuccess.animate().scaleXBy(900f).duration = 800L
         binding.vSuccess.animate().scaleYBy(900f).duration = 800L
+
+        delay(500L)
+        binding.ivSuccess.animate().alpha(1f).duration = 1000L
+
+        delay(1500L)
+    }
+
+    private fun navigateToSuccess(){
+        findNavController().navigate(R.id.action_homeFragment_to_successFragment)
     }
 
     // for avoiding memory leaks set binding to null
